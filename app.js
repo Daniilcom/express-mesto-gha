9c21ecad65eabc257f805b42b7361a3a24ac8cf3
daniilcom <dan19.95@mail.ru>
2023-07-22 19:58:32 +0300

    feat: refactoring after review

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const mongoose = require('mongoose');
 
 const { ERROR_NOT_FOUND } = require('./utils/constants');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
 
 app.use(express.json());
-
+app.use(helmet());
 app.use((req, res, next) => {
   req.user = {
     _id: '64ba66017bbce797d7e6b159',
