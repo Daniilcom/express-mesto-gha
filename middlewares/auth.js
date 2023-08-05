6cf4a3676d6f95c0d943a239e4e263e858d7d669
daniilcom <dan19.95@mail.ru>
2023-08-06 01:00:48 +0300

    fix: test15

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,16 @@ const AuthError = require('../utils/errors/auth-err');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new AuthError('Необходимо авторизоваться'));
+    next(new AuthError('Необходимо авторизоваться'));
+    return;
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return next(new AuthError('Необходимо авторизоваться'));
+    next(new AuthError('Необходимо авторизоваться'));
+    return;
   }
   req.user = payload;
   next();
