64b403cd50e3bb6e271afa3ac79c111bfd1801e8
daniilcom <dan19.95@mail.ru>
2023-08-05 19:44:32 +0300

    feat: auth

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,9 @@
-const ERROR_CODE = 400;
-const ERROR_NOT_FOUND = 404;
-const ERROR_SERVER = 500;
 const SUCCESS_CODE = 200;
 const CREATED_CODE = 201;
+const RegExp = /^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/;
 
 module.exports = {
-  ERROR_CODE,
-  ERROR_NOT_FOUND,
-  ERROR_SERVER,
   SUCCESS_CODE,
   CREATED_CODE,
+  RegExp,
 };
